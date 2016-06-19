ebb84cb6840fd0124f478f47e91632783f3ae67a
Yuto Otaguro <yotagur@gmail.com>
2016-06-18 22:51:40 -0500

    put word cloud in the bin for the moment
    seems to be more work than anticipated

--- src/base.js
+++ src/base.js
@@ -3,15 +3,9 @@
 var Discord = require('discord.js');
 var bot = new Discord.Client({ queue : true });
 
-var initialHandler = require('./Handlers/initial');
 var presenceHandler = require('./Handlers/presence');
 
 var words = require('../src/counter');
-
-// Run the init function to set up the words dictionary.
-bot.on('ready', function() {
-    initialHandler(bot);
-});
 
 // When someone's status changes, run a function.
 bot.on('presence', presenceHandler);
