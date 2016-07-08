8931a318d12616af1133e36c2394d2a164f6e7d0
Yuto Otaguro <yotagur@gmail.com>
2016-07-08 11:50:04 -0500

    wrong flag

--- src/tools/user_methods.js
+++ src/tools/user_methods.js
@@ -23,7 +23,7 @@ function getGameName(user) {
     var game = user.game;
 
     // If there is a game object, then return the name.
-    if (game && user.bot) {
+    if (game && !user.bot) {
         return game.name;
     }
 
