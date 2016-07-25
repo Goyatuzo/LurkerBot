6f55f52339e6cb9d17d2c33eb3f310a12cfce2c9
Yuto Otaguro <yotagur@gmail.com>
2016-07-24 20:15:31 -0500

    testing environment works on windows

--- Bot/gulpfile.js
+++ Bot/gulpfile.js
@@ -3,6 +3,9 @@ var ts = require("gulp-typescript");
 var tsProject = ts.createProject("tsconfig.json");
 
 gulp.task('default', function () {
+    var res = gulp.src('./resources/*')
+    .pipe(gulp.dest("build/resources"));
+
     return tsProject.src()
     .pipe(ts(tsProject))
     .js.pipe(gulp.dest("build"));
