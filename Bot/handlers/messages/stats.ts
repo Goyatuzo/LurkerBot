7d23e9b9c5532efbdbdf0dce476e49d871e6ee6f
Yuto Otaguro <yotagur@gmail.com>
2016-07-31 22:20:07 -0500

    Revert "moved hosting to azure"
    This reverts commit d7a685b01e520174e57a92fc8437e392484fcaea.

---
+++ Bot/handlers/messages/stats.ts
@@ -0,0 +1,42 @@
+﻿import {Message, Client, TextChannel} from "discord.js";
+import * as _ from "lodash";
+
+import {getDurationSumTimeSorted} from "../../database/times_table";
+
+function _stringifyTime(seconds) {
+    var m = Math.floor(seconds / 60);
+    var secs = seconds % 60;
+
+    var h = Math.floor(m / 60);
+    var minutes = m % 60;
+
+    var days = Math.floor(h / 24);
+    var hours = Math.floor(h % 24);
+    return days + ' days ' + hours + ' hours ' + minutes + ' minutes ' + secs + ' seconds';
+}
+
+function _formatResults(results) {
+    let summary = "";
+
+    results.map(row => {
+        summary += `${row.name}\n - ${_stringifyTime(row.duration) }\n`;
+    });
+    return summary;
+}
+
+export default function (message: Message) {
+    const tokens = _.drop(message.cleanContent.split(" "));
+    const client = message.client;
+
+    const sortTerm = _.drop(message.cleanContent.split(" sortby "))[0];
+
+    // If the user only requests stats, then print out the server's stats.
+    if (tokens.length === 0) {
+        const users = (message.channel as TextChannel).server.members;
+        const userIds = users.map(user => user);
+        
+        getDurationSumTimeSorted(userIds, results => {
+            client.sendMessage(message.channel, _formatResults(results));
+        });
+    }
+}
---
+++ Bot/typings/index.d.ts
@@ -0,0 +1,7 @@
+/// <reference path="globals/discord.js/index.d.ts" />
+/// <reference path="globals/mocha/index.d.ts" />
+/// <reference path="globals/mysql/index.d.ts" />
+/// <reference path="globals/node-4/index.d.ts" />
+/// <reference path="modules/chai/index.d.ts" />
+/// <reference path="modules/lodash/index.d.ts" />
+/// <reference path="modules/mkdirp/index.d.ts" />
