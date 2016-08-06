51d2264ed0d03c852b33448647142a0d692895ee
Yuto Otaguro <yotagur@gmail.com>
2016-08-05 21:24:17 -0500

    Reconfigured project for Azure hosting.

--- Web/api/times.ts
+++ Web/api/times.ts
@@ -1,4 +1,5 @@
 ﻿import connection from './connection';
+import ITime from '../classes/i-times';
 
 /**
  * Get the list of the summary of times by users.
@@ -8,7 +9,7 @@ export function getTimesByUser(callback) {
     const stmt: string = `SELECT U.name, T.gameName, SUM(T.duration) AS duration FROM Immutable.Times T JOIN lurkerbot.Users U ON T.id = U.id GROUP BY T.gameName, U.name ORDER BY T.gameName`;
 
     const start: Date = new Date();
-    connection.query(stmt, (err, results) => {
+    connection.query(stmt, (err, results: Array<ITime>) => {
         if (err) {
             console.log(err);
             return;
---
+++ Web/classes/i-times.ts
@@ -0,0 +1,11 @@
+﻿/**
+ * For the purposes of the API, GameTime represents the table where the entries
+ * where the sum of each game times for each user is accumulated.
+ */
+interface ITime {
+    name: string;
+    gameName: string;
+    duration: number;
+}
+
+export default ITime;
