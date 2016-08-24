2bab1f884debb56669e043d3094bd5c593d02b86
Yuto Otaguro <yotagur@gmail.com>
2016-08-23 21:38:43 -0500

    Shows URL to user stats. - Changed some vars to const.

--- Bot/handlers/messages/stats.ts
+++ Bot/handlers/messages/stats.ts
@@ -4,14 +4,14 @@ import * as _ from "lodash";
 import {getDurationSumTimeSorted} from "../../database/times_table";
 
 function _stringifyTime(seconds) {
-    var m = Math.floor(seconds / 60);
-    var secs = seconds % 60;
+    const m = Math.floor(seconds / 60);
+    const secs = seconds % 60;
 
-    var h = Math.floor(m / 60);
-    var minutes = m % 60;
+    const h = Math.floor(m / 60);
+    const minutes = m % 60;
 
-    var days = Math.floor(h / 24);
-    var hours = Math.floor(h % 24);
+    const days = Math.floor(h / 24);
+    const hours = Math.floor(h % 24);
     return days + ' days ' + hours + ' hours ' + minutes + ' minutes ' + secs + ' seconds';
 }
 
@@ -28,16 +28,15 @@ export default function (message: Message) {
     const tokens = _.drop(message.cleanContent.split(" "));
     const client = message.client;
 
-    const sortTerm = _.drop(message.cleanContent.split(" sortby "))[0];
-
     // If the user only requests stats, then print out the server's stats.
     if (tokens.length === 0) {
         const users = (message.channel as TextChannel).server.members;
         const userIds = users.map(user => user);
         
         getDurationSumTimeSorted(userIds, results => {
-            client.sendMessage(message.channel, _formatResults(results));
             client.sendMessage(message.channel, `Visit http://lurkerbot.azurewebsites.net/query?serverId=${message.server.id} for a graphical summary.`);
         });
+    } else if (tokens[0] === "me") {
+        client.sendMessage(message.channel, `Visit http://lurkerbot.azurewebsites.net/query?userId=${message.author.id} for a graphical summary.`);
     }
 }
