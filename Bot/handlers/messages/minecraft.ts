b128d2a0280fe5b3cac87da5b533408cb5c71735
Yuto Otaguro <yotagur@gmail.com>
2017-02-19 15:03:38 -0600

    Print out detailed death listing.

--- Bot/handlers/messages/minecraft.ts
+++ Bot/handlers/messages/minecraft.ts
@@ -5,6 +5,47 @@ import McFtp from '../../tools/ftp/connect';
 import mcData from '../../tools/minecraft/storage';
 
 export default function (message: Message): void {
+    const client = message.client;
+    const msgTokens = message.cleanContent.split(' ');
+
+    if (!msgTokens[2]) {
+
+    } else if (msgTokens[2].toLowerCase() === "deaths") {
+        processMinecraftData(message, deathSummary);
+    } else if (msgTokens[2].toLowerCase() === "death-details") {
+        processMinecraftData(message, deathDetails);
+    }
+}
+
+function deathDetails(): string {
+    let deathString = "";
+
+    mcData.deathsArray.forEach(death => {
+        deathString += `${death.playerName}\n`;
+
+        death.causes.forEach(deathCause => {
+            deathString += `\t- ${deathCause}\n`;
+        });
+    });
+
+    mcData.clearCache();
+
+    return deathString;
+}
+
+function deathSummary(): string {
+    let deathString = "";
+
+    mcData.deathsArray.forEach(death => {
+        deathString += `${death.playerName} has died ${death.causes.length} times\n`;
+    });
+
+    mcData.clearCache();
+
+    return deathString;
+}
+
+function processMinecraftData(message: Message, processData: () => string): void {
     McFtp.connection.get('server.log', (error, fileStream) => {
         if (error) {
             console.error(error);
@@ -20,15 +61,7 @@ export default function (message: Message): void {
         });
 
         readLine.on('close', () => {
-            let deathString = "";
-            
-            mcData.deathsArray.forEach(death => {
-                deathString += `${death.playerName} has died ${death.causes.length} times\n`;
-            });
-
-            mcData.clearCache();
-
-            message.channel.sendMessage(deathString).catch(error => {
+            message.channel.sendMessage(processData()).catch(error => {
                 console.error(error);
             });
         });
