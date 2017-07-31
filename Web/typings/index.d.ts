480d507348f3a16cbc76d7e43accd7a568b6adb4
Yuto Otaguro <yotagur@gmail.com>
2017-07-30 23:04:54 -0500

    Isolate web project.

--- Web/typings/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="globals/highcharts/index.d.ts" />
-/// <reference path="globals/jquery/index.d.ts" />
-/// <reference path="globals/lodash/index.d.ts" />
-/// <reference path="globals/mysql/index.d.ts" />
