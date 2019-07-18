1d8c7032e4f78c6d520c715c1e367c2bcc2530d8
Yuto Otaguro <yotagur@gmail.com>
2019-07-18 15:57:57 -0500

    Differentiate text and DM channels

--- bot/handlers/events/message.ts
+++ bot/handlers/events/message.ts
@@ -1,16 +1,18 @@
-﻿import { Message, TextChannel, Client } from "discord.js";
+﻿import { Message, TextChannel, Client, DMChannel } from "discord.js";
 import * as _ from "lodash";
 
-import messageRouter from "../messages";
+import messageRouter from "../messages/text";
 
 export default function (bot: Client) {
-    return function(message: Message): void {
-        console.log(message);
+    return function (message: Message): void {
+        if (message.channel instanceof TextChannel) {
+            // If the message dosen't begin with an at mention at lurkerbot, just ignore it.
+            if (!message.cleanContent.startsWith(`!${bot.user.username.toLocaleLowerCase()}`))
+                return;
 
-        // If the message dosen't begin with an at mention at lurkerbot, just ignore it.
-        if (!message.cleanContent.startsWith(`!${bot.user.username.toLocaleLowerCase()}`))
-            return;
-
-        messageRouter(message);
+            messageRouter(message);
+        } else if (message.channel instanceof DMChannel) {
+            console.log("DM");
+        }
     }
 } 
