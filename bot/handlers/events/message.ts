57cf2bde6b747c85e66653a6434bd068eb3ef858
Yuto Otaguro <yotagur@gmail.com>
2019-07-18 15:50:40 -0500

    Misssed one

--- bot/handlers/events/message.ts
+++ bot/handlers/events/message.ts
@@ -5,6 +5,8 @@ import messageRouter from "../messages";
 
 export default function (bot: Client) {
     return function(message: Message): void {
+        console.log(message);
+
         // If the message dosen't begin with an at mention at lurkerbot, just ignore it.
         if (!message.cleanContent.startsWith(`!${bot.user.username.toLocaleLowerCase()}`))
             return;
