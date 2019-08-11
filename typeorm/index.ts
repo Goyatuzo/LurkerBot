6a4d326f89dc3edec61a339262d873b082e6ddeb
Yuto Otaguro <yotagur@gmail.com>
2019-08-11 12:15:34 -0500

    Create a secondary models area for pm2

--- typeorm/index.ts
+++ typeorm/index.ts
@@ -1,6 +1,7 @@
 import { ConnectionOptions } from 'typeorm';
 import * as fs from 'fs';
 import { Configuration } from '../helpers/environment';
+import * as path from 'path';
 
 console.log(Configuration.DB_HOST)
 
@@ -11,7 +12,8 @@ export const connectionOptions: ConnectionOptions = {
     logging: false,
     useNewUrlParser: true,
     entities: [
-        "typeorm/models/**/*.ts"
+        "typeorm/models/**/*.ts",
+        path.join(__dirname, "models/**/*.ts")
     ],
     migrations: [
         "typeorm/migration/**/*.ts"
