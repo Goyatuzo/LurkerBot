90bdddb43bc0ce00a67226ab84708be26392237d
Yuto Otaguro <yotagur@gmail.com>
2019-08-11 11:02:04 -0500

    Don't allow duplicate useres to add themselves

--- bot/handlers/messages/dm/add-me.ts
+++ bot/handlers/messages/dm/add-me.ts
@@ -1,18 +1,23 @@
 import { DiscordDBUser } from "../../../../typeorm/models/discord-db-user";
 import { Message, User } from "discord.js";
-import {  getMongoRepository } from "typeorm";
+import { getMongoRepository } from "typeorm";
 
 export default function addMeHandler(message: Message) {
     const discordUser = message.author;
     const userRepository = getMongoRepository(DiscordDBUser);
 
-    const newUser = new DiscordDBUser();
-    newUser.userId = discordUser.id;
-    newUser.discriminator = discordUser.discriminator;
-    newUser.username = discordUser.username;
-
-    userRepository.save(newUser);
-
-    console.log(`Added ${discordUser.username}#${discordUser.discriminator}`);
-    message.reply(`${discordUser.username}#${discordUser.discriminator} has been added to the user list. Your game time stats are now being recorded.`);
+    userRepository.findOneOrFail({ userId: discordUser.id }).then(match => {
+        console.log(`${discordUser.username}#${discordUser.discriminator} already being tracked`);
+        message.reply(`You're already being tracked. No action was taken at this time.`);
+    }).catch(err => {
+        const newUser = new DiscordDBUser();
+        newUser.userId = discordUser.id;
+        newUser.discriminator = discordUser.discriminator;
+        newUser.username = discordUser.username;
+    
+        userRepository.save(newUser);
+    
+        console.log(`Added ${discordUser.username}#${discordUser.discriminator}`);
+        message.reply(`${discordUser.username}#${discordUser.discriminator} has been added to the user list. Your game time stats are now being recorded.`);
+    });
 }
