9cdccb8799d754f6379fbdf45a30ee5896414aa4
Yuto OTAGURO <yuto@Yutos-Mac-mini.local>
2019-08-20 20:22:30 -0500

    Display proper error messaging for bad cmds

--- bot/handlers/messages/dm/index.ts
+++ bot/handlers/messages/dm/index.ts
@@ -5,9 +5,16 @@ export default function (message: Message) {
     const client = message.client;
     const msgTokens = message.cleanContent.split(' ');
 
+    // If the author of the message is a bot, then ignore.
+    if (message.author.bot) {
+        return;
+    }
+
     if (message.cleanContent.toLowerCase() === "addme") {
         addMeHandler(message);
     } else if (message.cleanContent.toLowerCase() === "removeme") {
         renmoveMeHandler(message);
+    } else {
+        message.reply(`Could not find an appropriate command that matches your query. At the moment, only 'addme' and 'removeme' are functional.`)
     }
 }
