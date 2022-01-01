1f95dfcf489d5cb67a3ebcc71ca36bce13df37d2
Goyatuzo <yotagur@gmail.com>
2022-01-01 00:36:47 -0600

    Remove pingpong

--- src/main/kotlin/LurkerBot.kt
+++ src/main/kotlin/LurkerBot.kt
@@ -1,6 +1,7 @@
 import dev.kord.core.Kord
 import dev.kord.core.entity.ReactionEmoji
 import dev.kord.core.event.message.MessageCreateEvent
+import dev.kord.core.event.user.PresenceUpdateEvent
 import dev.kord.core.on
 import dev.kord.gateway.Intent
 import dev.kord.gateway.Intents
@@ -9,17 +10,9 @@ import kotlinx.coroutines.delay
 
 suspend fun main() {
     val client = Kord(System.getenv("LURKER_BOT_TOKEN"))
-    val pingPong = ReactionEmoji.Unicode("\uD83C\uDFD3")
 
-    client.on<MessageCreateEvent> {
-        if (message.content != "!ping") return@on
-
-        val response = message.channel.createMessage("Pong!")
-        response.addReaction(pingPong)
-
-        delay(5000)
-        message.delete()
-        response.delete()
+    client.on<PresenceUpdateEvent> {
+        println(this)
     }
 
     client.login {
---
+++ src/main/kotlin/gameTime/GameTimer.kt
@@ -0,0 +1,4 @@
+package gameTime
+
+class GameTimer {
+}
