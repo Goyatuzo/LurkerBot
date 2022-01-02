2bb4a91ba49ba8e0f0703c68ea54628384e62dde
Goyatuzo <yotagur@gmail.com>
2022-01-02 15:29:14 -0600

    Implement save

--- src/main/kotlin/gameTime/TimerRepository.kt
+++ src/main/kotlin/gameTime/TimerRepository.kt
@@ -1,11 +1,15 @@
 package gameTime
 
+import org.litote.kmongo.*
 import com.mongodb.client.MongoClient
 
 data class TimerRepository(
     private val mongoClient: MongoClient
 ) {
     fun saveTimeRecord(record: TimeRecord) {
+        val database = mongoClient.getDatabase("lurker-bot")
+        val collection = database.getCollection<TimeRecord>("game_time")
 
+        collection.insertOne(record)
     }
 }
