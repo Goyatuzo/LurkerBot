71f6ef58be4979a5a30e41e847a1ce31d13bc852
Yuto Otaguro <yotagur@gmail.com>
2022-01-02 00:11:15 -0600

    Create test for error

--- src/test/kotlin/gameTime/GameTimerTest.kt
+++ src/test/kotlin/gameTime/GameTimerTest.kt
@@ -13,20 +13,21 @@ class GameTimerTest {
 
     val gameTimer = GameTimer(timerRepository)
 
+    val basicTimeRecord = TimeRecord(
+        sessionBegin = LocalDate.now(),
+        sessionEnd = LocalDate.MAX,
+        gameName = "game",
+        userId = "test",
+        gameDetail = "Detail",
+        gameState = "State",
+        largeAssetText = "",
+        smallAssetText = "",
+        gameType = ""
+    )
+
     @Test
     fun `Should be able to start logging a brand new game and end it`() {
-        val toInsert = TimeRecord(
-            sessionBegin = LocalDate.now(),
-            sessionEnd = LocalDate.MAX,
-            gameName = "game",
-            userId = "test",
-            gameDetail = "Detail",
-            gameState = "State",
-            largeAssetText = "",
-            smallAssetText = "",
-            gameType = ""
-        )
-
+        val toInsert = basicTimeRecord.copy()
         every { timerRepository.saveTimeRecord(any()) } returns Unit
 
         gameTimer.beginLogging("test", "game", toInsert)
@@ -38,5 +39,13 @@ class GameTimerTest {
         }
 
         confirmVerified(timerRepository)
+    }
+
+    @Test
+    fun `Should not be be able to end logging that never started`() {
+        every { timerRepository.saveTimeRecord(any()) } returns Unit
+
+        val actual = gameTimer.endLogging("test", "game")
+        assertThat(actual).isNull()
     }
 }
