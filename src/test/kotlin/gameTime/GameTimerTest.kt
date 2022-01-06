4b580b7a725b9219c83616d4fc5d7a4e0d10e4f1
Goyatuzo <yotagur@gmail.com>
2022-01-05 19:42:03 -0600

    Verify insert was only called once

--- src/test/kotlin/gameTime/GameTimerTest.kt
+++ src/test/kotlin/gameTime/GameTimerTest.kt
@@ -52,11 +52,61 @@ class GameTimerTest {
     }
 
     @Test
+    fun `When two servers start logging for same user, only the first is added`() {
+        val firstToInsert = basicTimeRecord.copy()
+
+        val fakeTime = LocalDateTime.of(2020, 1, 1, 1, 0, 0)
+        val secondToInsert = basicTimeRecord.copy(sessionBegin = fakeTime)
+        every { timerRepository.saveTimeRecord(any()) } returns Unit
+
+        gameTimer.beginLogging("test", "test server", firstToInsert)
+        val error = gameTimer.beginLogging("test", "test server 2", secondToInsert)
+
+        assertThat(error)
+            .isEqualTo(Err(GameIsAlreadyLogging("test", firstToInsert, secondToInsert)))
+    }
+
+    @Test
+    fun `When two servers start logging for same user and ends, only the first is saved to database`() {
+        val firstToInsert = basicTimeRecord.copy()
+        val now = LocalDateTime.now()
+
+        val fakeTime = LocalDateTime.of(2020, 1, 1, 1, 0, 0)
+        val secondToInsert = basicTimeRecord.copy(sessionBegin = fakeTime)
+        every { timerRepository.saveTimeRecord(any()) } returns Unit
+
+        gameTimer.beginLogging("test", "test server", firstToInsert)
+        gameTimer.beginLogging("test", "test server 2", secondToInsert)
+
+        gameTimer.endLogging("test", "test server", now)
+        gameTimer.endLogging("test", "test server", now)
+
+        verify(exactly = 1) {
+            timerRepository.saveTimeRecord(record = firstToInsert.copy(sessionEnd = now))
+            timerRepository.saveTimeRecord(record = not(secondToInsert.copy(sessionEnd = now)))
+        }
+
+        confirmVerified(timerRepository)
+    }
+
+    @Test
     fun `Should not be be able to save logging that never started`() {
         every { timerRepository.saveTimeRecord(any()) } returns Unit
 
-        val actual = gameTimer.endLogging("test", "test server",)
-        assertThat(actual).isEqualTo(Err(NeverStartedLogging("test", "test server",)))
+        val actual =
+            gameTimer.endLogging(
+                "test",
+                "test server",
+            )
+        assertThat(actual)
+            .isEqualTo(
+                Err(
+                    NeverStartedLogging(
+                        "test",
+                        "test server",
+                    )
+                )
+            )
 
         verify { timerRepository wasNot Called }
 
