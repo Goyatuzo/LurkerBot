c7bfd4887c3203442848f5a1b11a0de2a5749423
Goyatuzo <yotagur@gmail.com>
2022-01-05 19:59:34 -0600

    Try Java 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,7 @@ tasks.test {
 }
 
 tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "1.17"
+    kotlinOptions.jvmTarget = "1.11"
 }
 
 tasks.withType<KotlinCompile>().configureEach {
