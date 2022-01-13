0708f0166637cd92d8e559806c06176422747a01
Yuto Otaguro <yotagur@gmail.com>
2022-01-13 06:50:45 +0000

    Move the mutex so that it only starts via critical section (#19)
    * Move the mutex so that it only starts via critical section
    
    * Removing mutex was ok?

--- src/main/kotlin/gameTime/GameTimeTracker.kt
+++ src/main/kotlin/gameTime/GameTimeTracker.kt
@@ -4,13 +4,10 @@ import com.lurkerbot.discordUser.UserTracker
 import dev.kord.common.entity.ActivityType
 import dev.kord.core.event.user.PresenceUpdateEvent
 import java.time.LocalDateTime
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
 import mu.KotlinLogging
 
 class GameTimeTracker(private val gameTimer: GameTimer, private val userTracker: UserTracker) {
     private val logger = KotlinLogging.logger {}
-    private val mutex = Mutex()
 
     suspend fun processEvent(event: PresenceUpdateEvent) {
         val user = event.getUser()
@@ -19,59 +16,52 @@ class GameTimeTracker(private val gameTimer: GameTimer, private val userTracker:
             return
         }
 
-        mutex.withLock {
-            if (!user.isBot) {
-                when (event.presence.activities.size) {
-                    0 -> {
-                        gameTimer.endLogging(
+        if (!user.isBot) {
+            when (event.presence.activities.size) {
+                0 -> {
+                    gameTimer.endLogging(user.id.value.toString(), event.guildId.value.toString())
+                }
+                else -> {
+                    val activity =
+                        event.presence.activities.firstOrNull { it.type == ActivityType.Game }
+
+                    if (activity != null) {
+                        event.old?.activities?.firstOrNull { it.type == ActivityType.Game }?.let {
+                            // Check some attributes for equality
+                            val sameActivity =
+                                it.name == activity.name &&
+                                    it.details == activity.details &&
+                                    it.assets?.smallText == activity.assets?.smallText &&
+                                    it.assets?.largeText == activity.assets?.largeText
+
+                            if (!sameActivity) {
+                                gameTimer.endLogging(
+                                    user.id.value.toString(),
+                                    event.guildId.value.toString()
+                                )
+                            }
+                        }
+                        val toRecord =
+                            TimeRecord(
+                                sessionBegin = LocalDateTime.now(),
+                                sessionEnd = LocalDateTime.now(),
+                                gameName = activity.name,
+                                userId = user.id.value.toString(),
+                                gameDetail = activity.details,
+                                gameState = activity.state,
+                                largeAssetText = activity.assets?.largeText,
+                                smallAssetText = activity.assets?.smallText
+                            )
+
+                        gameTimer.beginLogging(
                             user.id.value.toString(),
-                            event.guildId.value.toString()
+                            event.guildId.value.toString(),
+                            toRecord
                         )
                     }
-                    else -> {
-                        val activity =
-                            event.presence.activities.firstOrNull { it.type == ActivityType.Game }
 
-                        if (activity != null) {
-                            event.old?.activities
-                                ?.firstOrNull { it.type == ActivityType.Game }
-                                ?.let {
-                                    // Check some attributes for equality
-                                    val sameActivity =
-                                        it.name == activity.name &&
-                                            it.details == activity.details &&
-                                            it.assets?.smallText == activity.assets?.smallText &&
-                                            it.assets?.largeText == activity.assets?.largeText
-
-                                    if (!sameActivity) {
-                                        gameTimer.endLogging(
-                                            user.id.value.toString(),
-                                            event.guildId.value.toString()
-                                        )
-                                    }
-                                }
-                            val toRecord =
-                                TimeRecord(
-                                    sessionBegin = LocalDateTime.now(),
-                                    sessionEnd = LocalDateTime.now(),
-                                    gameName = activity.name,
-                                    userId = user.id.value.toString(),
-                                    gameDetail = activity.details,
-                                    gameState = activity.state,
-                                    largeAssetText = activity.assets?.largeText,
-                                    smallAssetText = activity.assets?.smallText
-                                )
-
-                            gameTimer.beginLogging(
-                                user.id.value.toString(),
-                                event.guildId.value.toString(),
-                                toRecord
-                            )
-                        }
-
-                        if (event.presence.activities.size > 1)
-                            logger.info { "Multiple activities: ${event.presence.activities}" }
-                    }
+                    if (event.presence.activities.size > 1)
+                        logger.info { "Multiple activities: ${event.presence.activities}" }
                 }
             }
         }
