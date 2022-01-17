d9d0b79de6f0173d8e2695d6d9cf99059b9cfba3
Yuto Otaguro <yotagur@gmail.com>
2022-01-17 23:50:06 +0000

    Change to discordkt (#21)
    * User simple logger and information logs
    
    * Suppress unused error
    
    * Fix unit tests
    
    * Fix unit tests

--- src/main/kotlin/LurkerBot.kt
+++ src/main/kotlin/LurkerBot.kt
@@ -1,35 +1,20 @@
 package com.lurkerbot
 
-import com.lurkerbot.discordUser.DiscordUserRepository
-import com.lurkerbot.discordUser.UserTracker
-import com.lurkerbot.gameTime.GameTimeTracker
-import com.lurkerbot.gameTime.GameTimer
-import com.lurkerbot.gameTime.TimerRepository
-import dev.kord.core.Kord
-import dev.kord.core.event.gateway.ReadyEvent
+import dev.kord.common.annotation.KordPreview
 import dev.kord.core.event.user.PresenceUpdateEvent
-import dev.kord.core.on
-import dev.kord.gateway.Intent
 import dev.kord.gateway.Intents
-import dev.kord.gateway.PrivilegedIntent
-import org.litote.kmongo.KMongo
+import me.jakejmattson.discordkt.api.dsl.bot
+import me.jakejmattson.discordkt.api.dsl.precondition
+import me.jakejmattson.discordkt.api.extensions.intentsOf
 
-suspend fun main() {
-    val client = Kord(System.getenv("LURKER_BOT_TOKEN"))
-    val mongoClient = KMongo.createClient(System.getenv("LURKER_BOT_DB"))
+@OptIn(KordPreview::class)
+fun main() {
+    bot(System.getenv("LURKER_BOT_TOKEN")) {
+        precondition { if (author.isBot) fail("Bots cannot invoke anything") }
 
-    val timerRepository = TimerRepository(mongoClient)
-    val gameTimer = GameTimer(timerRepository)
-    val userRepository = DiscordUserRepository(mongoClient)
-    val userTracker = UserTracker(userRepository)
-    val gameTimeTracker = GameTimeTracker(gameTimer, userTracker)
-
-    client.on<PresenceUpdateEvent> {
-        gameTimeTracker.processEvent(this)
-    }
-
-    client.login {
-        @OptIn(PrivilegedIntent::class)
-        intents = Intents.nonPrivileged + Intent.GuildPresences
+        configure {
+            intents = Intents.nonPrivileged + intentsOf<PresenceUpdateEvent>()
+            generateCommandDocs = false
+        }
     }
 }
