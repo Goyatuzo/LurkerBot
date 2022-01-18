e99efde76af0d521814a928e73906b364c25d939
Goyatuzo <yotagur@gmail.com>
2022-01-17 22:38:55 -0600

    kotlin way of implementing singleton

--- src/main/kotlin/internal/MongoDatabase.kt
+++ src/main/kotlin/internal/MongoDatabase.kt
@@ -3,11 +3,6 @@ package com.lurkerbot.internal
 import com.mongodb.client.MongoClient
 import org.litote.kmongo.KMongo
 
-class MongoDatabase {
-    companion object {
-        private val mongoClient: MongoClient = KMongo.createClient(System.getenv("LURKER_BOT_DB"))
-
-        val client: MongoClient
-            get() = mongoClient
-    }
+object MongoDatabase {
+    val client: MongoClient = KMongo.createClient(System.getenv("LURKER_BOT_DB"))
 }
