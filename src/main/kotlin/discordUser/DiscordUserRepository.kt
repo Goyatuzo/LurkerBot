daa6d53f98a4f1af53450c101e59761164d700cd
Goyatuzo <yotagur@gmail.com>
2022-01-17 21:39:25 -0600

    Log user insert / delete events

--- src/main/kotlin/discordUser/DiscordUserRepository.kt
+++ src/main/kotlin/discordUser/DiscordUserRepository.kt
@@ -2,12 +2,15 @@ package com.lurkerbot.discordUser
 
 import com.mongodb.client.MongoClient
 import com.mongodb.client.MongoCollection
+import mu.KotlinLogging
 import org.litote.kmongo.eq
 import org.litote.kmongo.findOne
 import org.litote.kmongo.getCollection
 import org.litote.kmongo.save
 
 data class DiscordUserRepository(private val mongoClient: MongoClient) {
+    private val logger = KotlinLogging.logger {}
+
     private fun getUserCollection(): MongoCollection<UserInDiscord> {
         val database = mongoClient.getDatabase("lurker-bot")
         return database.getCollection<UserInDiscord>("discord_db_user")
@@ -22,10 +25,12 @@ data class DiscordUserRepository(private val mongoClient: MongoClient) {
     fun saveUserInDiscord(user: UserInDiscord) {
         val collection = getUserCollection()
         collection.save(user)
+        logger.info { "Saved new user: ${user.username}#${user.discriminator}" }
     }
 
     fun removeUserInDiscord(user: UserInDiscord) {
         val collection = getUserCollection()
         collection.deleteOne(UserInDiscord::userId eq user.userId)
+        logger.info { "Deleting new user: ${user.username}#${user.discriminator}" }
     }
 }
