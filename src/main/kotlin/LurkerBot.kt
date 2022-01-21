d8d9ae5f88d761fb0fa069dcebb2185607a4b66f
Goyatuzo <yotagur@gmail.com>
2022-01-20 19:18:56 -0600

    Remove bot precondition

--- src/main/kotlin/LurkerBot.kt
+++ src/main/kotlin/LurkerBot.kt
@@ -4,14 +4,11 @@ import dev.kord.common.annotation.KordPreview
 import dev.kord.core.event.user.PresenceUpdateEvent
 import dev.kord.gateway.Intents
 import me.jakejmattson.discordkt.dsl.bot
-import me.jakejmattson.discordkt.dsl.precondition
 import me.jakejmattson.discordkt.extensions.intentsOf
 
 @OptIn(KordPreview::class)
 fun main() {
     bot(System.getenv("LURKER_BOT_TOKEN")) {
-        precondition { if (author.isBot) fail("Bots cannot invoke anything") }
-
         configure {
             intents = Intents.nonPrivileged + intentsOf<PresenceUpdateEvent>()
             generateCommandDocs = false
