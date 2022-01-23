50b67c920591d9678f1494130f667d9cfaaa7037
Goyatuzo <yotagur@gmail.com>
2022-01-23 01:06:13 -0600

    CollectLatest did not have desired output

--- src/main/kotlin/gameTime/GameTimeTracker.kt
+++ src/main/kotlin/gameTime/GameTimeTracker.kt
@@ -4,7 +4,7 @@ import com.lurkerbot.discordUser.UserTracker
 import dev.kord.common.entity.ActivityType
 import dev.kord.core.event.gateway.ReadyEvent
 import dev.kord.core.event.user.PresenceUpdateEvent
-import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.collectIndexed
 import mu.KotlinLogging
 
 class GameTimeTracker(private val gameTimer: GameTimer, private val userTracker: UserTracker) {
@@ -13,8 +13,8 @@ class GameTimeTracker(private val gameTimer: GameTimer, private val userTracker:
     suspend fun initialize(readyEvent: ReadyEvent) {
         val guilds = readyEvent.getGuilds()
 
-        guilds.collectLatest { guild ->
-            guild.members.collectLatest { member ->
+        guilds.collectIndexed { _, guild ->
+            guild.members.collectIndexed { _, member ->
                 if (userTracker.userIsBeingTracked(member.id.toString())) {
                     val gameActivity =
                         member.getPresenceOrNull()?.activities?.firstOrNull {
