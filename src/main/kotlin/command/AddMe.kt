1d7a40a04c6d21e2bc937d47ced2d9ba3b30bef0
Goyatuzo <yotagur@gmail.com>
2022-01-25 00:06:02 -0600

    Update descriptions

--- src/main/kotlin/command/AddMe.kt
+++ src/main/kotlin/command/AddMe.kt
@@ -9,7 +9,7 @@ class AddMe(private val userTracker: UserTracker) : BotCommand {
     private val logger = KotlinLogging.logger {}
 
     override val name: String = "add-me"
-    override val description: String = "Descriptive"
+    override val description: String = "Start tracking the time you play games. If you're already tracking, update your username."
     override suspend fun invoke(interaction: ApplicationCommandInteraction) {
         val user = interaction.user.asUserOrNull()
         if (user != null) {
