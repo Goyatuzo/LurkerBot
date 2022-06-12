0ffc330049914ab638de37f7541c45b21b3eb6cd
Goyatuzo <yotagur@gmail.com>
2022-06-12 00:49:42 -0500

    Versions are updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,16 +12,16 @@ repositories {
     mavenCentral()
 }
 
-val log4jVersion = "2.17.1"
+val log4jVersion = "2.17.2"
 
 dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test:1.6.0")
     testImplementation("com.google.truth:truth:1.1.3")
-    testImplementation("io.mockk:mockk:1.12.3")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
-    implementation("dev.kord:kord-core:0.8.0-M8")
-    implementation("org.litote.kmongo:kmongo:4.5.0")
-    implementation("com.michael-bull.kotlin-result:kotlin-result:1.1.14")
+    testImplementation("io.mockk:mockk:1.12.4")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
+    implementation("dev.kord:kord-core:0.8.0-M14")
+    implementation("org.litote.kmongo:kmongo:4.5.1")
+    implementation("com.michael-bull.kotlin-result:kotlin-result:1.1.16")
     implementation("org.apache.logging.log4j:log4j-core:$log4jVersion")
     implementation("org.apache.logging.log4j:log4j-api:$log4jVersion")
     implementation("org.apache.logging.log4j:log4j-slf4j-impl:$log4jVersion")
