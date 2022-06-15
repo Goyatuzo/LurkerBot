18019787eb03b1000005e4b62495f5e27995d017
Goyatuzo <yotagur@gmail.com>
2022-06-15 00:10:26 -0500

    Create function to get sum of game time

--- plyd-core/src/main/kotlin/gameTime/TimerRepository.kt
+++ plyd-core/src/main/kotlin/gameTime/TimerRepository.kt
@@ -2,4 +2,5 @@ package gameTime
 
 interface TimerRepository {
     fun saveTimeRecord(record: TimeRecord)
+    fun getSummedTimeRecordsFor(userId: String): List<TimeRecord>
 }
--- plyd-mongodb/src/main/kotlin/gameTime/KMongoTimerRepository.kt
+++ plyd-mongodb/src/main/kotlin/gameTime/KMongoTimerRepository.kt
@@ -1,17 +1,34 @@
 package gameTime
 
 import com.mongodb.client.MongoClient
+import com.mongodb.client.MongoCollection
 import mu.KotlinLogging
-import org.litote.kmongo.getCollection
+import org.litote.kmongo.*
 
 class KMongoTimerRepository(private val mongoClient: MongoClient) : TimerRepository {
     private val logger = KotlinLogging.logger {}
 
-    override fun saveTimeRecord(record: TimeRecord) {
+    private fun getCollection(): MongoCollection<TimeRecord> {
         val database = mongoClient.getDatabase("lurker-bot")
-        val collection = database.getCollection<TimeRecord>("game_time")
+        return database.getCollection<TimeRecord>("game_time")
+    }
+
+    override fun saveTimeRecord(record: TimeRecord) {
+        val collection = getCollection()
 
         collection.insertOne(record)
         logger.info { "Inserted: $record" }
+    }
+
+    override fun getSummedTimeRecordsFor(userId: String): List<TimeRecord> {
+        val collection = getCollection()
+
+        val aggregate = collection.aggregate(listOf(
+            match(TimeRecord::userId eq userId)
+        ))
+
+        println(aggregate.json)
+
+        return aggregate.toList()
     }
 }
