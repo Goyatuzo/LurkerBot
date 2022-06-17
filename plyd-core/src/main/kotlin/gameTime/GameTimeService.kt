7bb77c7b8c687560c6736de6ade2153eb3fd8122
Goyatuzo <yotagur@gmail.com>
2022-06-16 22:53:56 -0500

    Spotless

--- plyd-core/src/main/kotlin/gameTime/GameTimeService.kt
+++ plyd-core/src/main/kotlin/gameTime/GameTimeService.kt
@@ -3,6 +3,8 @@ package gameTime
 import java.time.LocalDateTime
 
 class GameTimeService(private val timerRepository: TimerRepository) {
-    fun getTimesForDiscordUserById(discordUserId: String, from: LocalDateTime = LocalDateTime.now().minusWeeks(2)) =
-        timerRepository.getSummedTimeRecordsFor(discordUserId, from)
+    fun getTimesForDiscordUserById(
+        discordUserId: String,
+        from: LocalDateTime = LocalDateTime.now().minusWeeks(2)
+    ) = timerRepository.getSummedTimeRecordsFor(discordUserId, from)
 }
--- plyd-core/src/main/kotlin/gameTime/TimerRepository.kt
+++ plyd-core/src/main/kotlin/gameTime/TimerRepository.kt
@@ -5,8 +5,5 @@ import response.GameTimeSum
 
 interface TimerRepository {
     fun saveTimeRecord(record: TimeRecord)
-    fun getSummedTimeRecordsFor(
-        userId: String,
-        from: LocalDateTime
-    ): List<GameTimeSum>
+    fun getSummedTimeRecordsFor(userId: String, from: LocalDateTime): List<GameTimeSum>
 }
