06e6d7cbb17634edd63fe8a28b9246fbe244bea2
Goyatuzo <yotagur@gmail.com>
2022-06-20 00:32:54 -0500

    Create endpoint for game filtering

--- web/src/main/kotlin/routes/PagesResource.kt
+++ web/src/main/kotlin/routes/PagesResource.kt
@@ -34,5 +34,30 @@ fun Application.configurePagesResource(pageService: PageService) {
                 }
             }
         }
+
+        get("/user/{discordUserId}/{gameName}") {
+            val userId = call.parameters["discordUserId"]
+            val gameName = call.parameters["gameName"]
+            val from = call.parameters["from"]
+
+            if (userId.isNullOrEmpty() || gameName.isNullOrEmpty()) {
+                call.respond(HttpStatusCode.NotFound)
+            } else {
+                val fromDate =
+                    if (from == "all") {
+                        LocalDateTime.of(0, 1, 1, 0, 0, 0)
+                    } else {
+                        LocalDateTime.now().minusWeeks(2)
+                    }
+
+                val userData = pageService.getTimesForDiscordUserByIdAndGame(userId, gameName, fromDate)
+
+                if (userData == null) {
+                    call.respond(HttpStatusCode.NotFound)
+                } else {
+                    call.respond(PebbleContent("pages/user-stats.html", mapOf("user" to userData)))
+                }
+            }
+        }
     }
 }
