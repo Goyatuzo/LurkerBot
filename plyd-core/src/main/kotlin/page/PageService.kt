615ec7536e8ec80fb13391a9163813f89c81d704
Yuto Otaguro <yotagur@gmail.com>
2022-06-24 06:43:43 +0000

    Game graphs (#45)
    * Get sorted game graph data
    
    * Generate deterministic hex
    
    * Remove rounding from DB
    
    * Display graphs
    
    * Remove debugging text

--- plyd-core/src/main/kotlin/page/PageService.kt
+++ plyd-core/src/main/kotlin/page/PageService.kt
@@ -4,9 +4,7 @@ import discordUser.UserService
 import gameTime.GameTimeService
 import gameTime.TimerRepository
 import java.time.LocalDateTime
-import response.RecentlyPlayed
-import response.UserTimeStats
-import response.UserTimeStatsByGame
+import response.*
 
 class PageService(
     private val userService: UserService,
@@ -26,6 +24,18 @@ class PageService(
         }
     }
 
+    private fun groupTimeByProperty(
+        stats: List<GameTimeDetailedSum>,
+        keySelector: (GameTimeDetailedSum) -> String?
+    ): List<TimeGraphData> =
+        stats
+            .groupBy(keySelector)
+            .mapNotNull {
+                if (it.key.isNullOrEmpty()) null
+                else TimeGraphData.of(it.key!!, it.value.sumOf { t -> t.time })
+            }
+            .sortedBy(TimeGraphData::time)
+
     fun getTimesForDiscordUserByIdAndGame(
         userId: String,
         gameName: String,
@@ -34,10 +44,22 @@ class PageService(
         val userInfo = userService.getUserByDiscordId(userId)
         val stats = timerRepository.getSummedGameTimeRecordsFor(userId, gameName, from)
 
+        val groupedByDetail = groupTimeByProperty(stats, GameTimeDetailedSum::detail)
+        val groupedByState = groupTimeByProperty(stats, GameTimeDetailedSum::state)
+        val groupedBySmallAsset = groupTimeByProperty(stats, GameTimeDetailedSum::smallAsset)
+        val groupedByLargeAsset = groupTimeByProperty(stats, GameTimeDetailedSum::largeAsset)
+
         return if (userInfo == null) {
             null
         } else {
-            UserTimeStatsByGame.of(userInfo, stats)
+            UserTimeStatsByGame.of(
+                userInfo = userInfo,
+                gameTime = stats,
+                byGameDetail = groupedByDetail,
+                byGameState = groupedByState,
+                bySmallAsset = groupedBySmallAsset,
+                byLargeAsset = groupedByLargeAsset
+            )
         }
     }
 }
--- plyd-core/src/main/kotlin/response/UserTimeStatsByGame.kt
+++ plyd-core/src/main/kotlin/response/UserTimeStatsByGame.kt
@@ -4,9 +4,30 @@ import discordUser.UserInDiscord
 
 @Suppress("DataClassPrivateConstructor")
 data class UserTimeStatsByGame
-private constructor(val userInfo: UserInDiscord, val gameTime: List<GameTimeDetailedSum>) {
+private constructor(
+    val userInfo: UserInDiscord,
+    val gameTime: List<GameTimeDetailedSum>,
+    val byGameState: List<TimeGraphData>,
+    val byGameDetail: List<TimeGraphData>,
+    val bySmallAsset: List<TimeGraphData>,
+    val byLargeAsset: List<TimeGraphData>
+) {
     companion object {
-        fun of(userInfo: UserInDiscord, gameTime: List<GameTimeDetailedSum>) =
-            UserTimeStatsByGame(userInfo, gameTime)
+        fun of(
+            userInfo: UserInDiscord,
+            gameTime: List<GameTimeDetailedSum>,
+            byGameDetail: List<TimeGraphData>,
+            byGameState: List<TimeGraphData>,
+            bySmallAsset: List<TimeGraphData>,
+            byLargeAsset: List<TimeGraphData>
+        ) =
+            UserTimeStatsByGame(
+                userInfo,
+                gameTime,
+                byGameState,
+                byGameDetail,
+                bySmallAsset,
+                byLargeAsset
+            )
     }
 }
