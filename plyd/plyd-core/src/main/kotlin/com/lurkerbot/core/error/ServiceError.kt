16c8205a8ab712431b6e6fd379f73b721ef09ea3
Goyatuzo <yotagur@gmail.com>
2022-08-31 00:33:45 -0500

    Create a bunch of Errors

--- plyd/plyd-core/src/main/kotlin/com/lurkerbot/core/error/ServiceError.kt
+++ plyd/plyd-core/src/main/kotlin/com/lurkerbot/core/error/ServiceError.kt
@@ -1,3 +1,11 @@
 package com.lurkerbot.core.error
 
+import com.lurkerbot.core.gameTime.TimeRecord
+
 object UserNotFound : DomainError()
+
+data class NeverStartedLogging(val userId: String, val guildId: String) : DomainError()
+
+data class StateChangedTooFast(val userId: String, val record: TimeRecord) : DomainError()
+
+data class GameIsAlreadyLogging(val userId: String, val newRecord: TimeRecord) : DomainError()
