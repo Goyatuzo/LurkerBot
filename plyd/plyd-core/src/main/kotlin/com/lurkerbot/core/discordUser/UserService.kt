7d2a948ccad50895d0ff46637bad65870f7bc6ac
Goyatuzo <yotagur@gmail.com>
2022-09-01 00:45:11 -0500

    Update user service to be Result

--- plyd/plyd-core/src/main/kotlin/com/lurkerbot/core/discordUser/UserService.kt
+++ plyd/plyd-core/src/main/kotlin/com/lurkerbot/core/discordUser/UserService.kt
@@ -1,6 +1,11 @@
 package com.lurkerbot.core.discordUser
 
+import com.github.michaelbull.result.Result
+import com.github.michaelbull.result.toResultOr
+import com.lurkerbot.core.error.DomainError
+import com.lurkerbot.core.error.UserNotFound
+
 class UserService(private val discordUserRepository: DiscordUserRepository) {
-    fun getUserByDiscordId(userId: String): UserInDiscord? =
-        discordUserRepository.getUserInDiscord(userId)
+    fun getUserByDiscordId(userId: String): Result<UserInDiscord, DomainError> =
+        discordUserRepository.getUserInDiscord(userId).toResultOr { UserNotFound }
 }
