a86013cce863d9bb0257e506bf61f73333f0db7f
Goyatuzo <yotagur@gmail.com>
2022-09-10 12:37:38 -0500

    Use unified kmongo version

--- plyd/plyd-dependencies/build.gradle.kts
+++ plyd/plyd-dependencies/build.gradle.kts
@@ -5,5 +5,5 @@ plugins {
 dependencies {
     implementation(project(":plyd:plyd-core"))
     implementation(project(":plyd:plyd-mongodb"))
-    implementation("org.litote.kmongo:kmongo:4.5.1")
+    implementation(dependency.kmongo)
 }
