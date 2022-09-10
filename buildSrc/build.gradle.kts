e8737c69833e18d7dc97a6a9da82cc96402cf7c7
Goyatuzo <yotagur@gmail.com>
2022-09-10 12:42:58 -0500

    Gradle plugin can't be unified

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,6 +7,6 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:${dependency.version.kotlin}")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
 }
 
